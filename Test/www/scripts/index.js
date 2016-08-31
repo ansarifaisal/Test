23ea82242d94c48f05df94f8c8139043c801e7bf
DrEaMeR <DrEaMeR@DESKTOP-N35M99C>
2016-08-31 23:51:47 +0530

    Add project files.

---
+++ Test/www/scripts/index.js
@@ -0,0 +1,40 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.getElementById("add").addEventListener('click',sum);
+    function sum() {
+        var num1 = document.frm.num1.value;
+        var num2 = document.frm.num2.value;
+        var sum = parseInt(num1)+ parseInt(num2);
+        console.log(sum);
+        document.getElementById("result").innerHTML = sum;
+
+    }
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )();
